94ce325bdf9d8c0b6420b181c9cbc3c6a34cf7e3
Alex978076 <joseantoniofilipealexandreviei@gmail.com>
2026-04-30 11:53:02 -0300

    Add files via upload

---
+++ Inventario de Laboratorio.ts
@@ -0,0 +1,14 @@
+interface Produto{
+    
+    id: number;
+  nome: string;
+  patrimonio: number;
+  emUso:true;
+}
+
+const produto:Produto = {
+    id:,
+    nome:,
+    patrimonio: ,
+    emUso:,
+}
