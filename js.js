ac40dd0d37ba703bc6cc377f719e1d24e6515ef6
Alex978076 <joseantoniofilipealexandreviei@gmail.com>
2026-05-31 17:17:00 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,40 +1,25 @@
-<<<<<<< HEAD
 let contador = 1;
-    document.getElementById("radio1").checked = true;
+const totalSlides = 4;
 
-    setInterval(function(){
-        contador++;
-        if(contador > 4){
-            contador = 1;
-        }
-        document.getElementById("radio" + contador). checked = true;
-    }, 3000);
-=======
+const radio1 = document.getElementById("radio1");
+if (radio1) radio1.checked = true;
+
+setInterval(function(){
+    contador++;
+    if (contador > totalSlides) {
+        contador = 1;
+    }
+    const radio = document.getElementById("radio" + contador);
+    if (radio) {
+        radio.checked = true;
+    }
+}, 3000);
+
 //Efect scroll 
 window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-const serviceModals = document.querySelector(".service-modal");
-const learnmoreBtns = document.querySelector(".learn-more-btn");
-const modalCloseBtns = document.querySelector(".modal-close-btn");
 
-var modal = function (modalClick) {
-    serviceModals[modalClick].classList.add("active");
-}
 
-learnmoreBtns.forEach((learnmoreBtn, i) => {
-    learnmoreBtn.addEventListener("click", () =>{
-        modal(i);
-    })
-})
-
-modalCloseBtns.forEach((modalCloseBtn) => {
-    modalCloseBtn.addEventListener("click", () => {
-         serviceModals.forEach((modalView) => {
-             modalView.classList.remove("active");
-         })
-    })
-})
->>>>>>> dcfb0966af8e9e7a4f7750d102c00838a8da4957
